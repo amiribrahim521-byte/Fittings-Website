8469435604fd103a43f945902dc2eed06b2bff2d
amiribrahim521-byte <amiribrahim521@gmail.com>
2025-11-02 16:28:24 +0500

    Today Updated Index file and styles.cc. Creating new js file

---
+++ main.js
@@ -0,0 +1,10 @@
+// SECTION 03
+
+// Initialize Swiper 
+var swiper = new Swiper(".mySwiper", {
+    loop : true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
