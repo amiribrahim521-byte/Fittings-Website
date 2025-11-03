1646700930bf699947cb2ae95f61c15f5e6f3051
amiribrahim521-byte <amiribrahim521@gmail.com>
2025-11-03 15:59:55 +0500

    HTML and Css work is Completed in index.html

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 var swiper = new Swiper(".mySwiper", {
     loop : true,
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl: "#next",
+        prevEl: "#prev",
     },
 });
